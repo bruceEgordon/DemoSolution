c7cc16b0a1e89e50785f575967b1b32c7c86a71f
Bruce Gordon <bruce@focusls.com>
2018-08-08 20:30:38 -0400

    1

---
+++ DemoSite/Models/Pages/StartPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace DemoSite.Models.Pages
+{
+    [ContentType(DisplayName = "Start Page", GUID = "2e595bed-e770-44c2-86dc-7b930d8a6421",
+        Description = "Use this to create the starting page for the site.", GroupName = "Specialized")]
+    public class StartPage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
