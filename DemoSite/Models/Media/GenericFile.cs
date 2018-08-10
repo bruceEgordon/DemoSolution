9838ac20cc78f68ab77044df5ec82343d72d3e18
Bruce Gordon <bruce@focusls.com>
2018-08-09 20:55:40 -0400

    1

---
+++ DemoSite/Models/Media/GenericFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace DemoSite.Models.Media
+{
+    [ContentType(DisplayName = "GenericFile", GUID = "b7d9380d-db8b-4722-a9c4-f7f102311ee5", Description = "")]
+    /*[MediaDescriptor(ExtensionString = "pdf,doc,docx")]*/
+    public class GenericFile : MediaData
+    {
+        /*
+                [CultureSpecific]
+                [Editable(true)]
+                [Display(
+                    Name = "Description",
+                    Description = "Description field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string Description { get; set; }
+         */
+    }
+}
---
+++ DemoSite/Models/Media/ImageFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+
+namespace DemoSite.Models.Media
+{
+    [ContentType(DisplayName = "ImageFile", GUID = "7163595e-aeb9-43e8-929a-9c251accf8c5", Description = "Use this to upload image files.")]
+    [MediaDescriptor(ExtensionString = "png,gif,jpg,jpeg")]
+    public class ImageFile : ImageData
+    {
+
+        [CultureSpecific]
+        [Editable(true)]
+        public virtual string Description { get; set; }
+
+    }
+}
