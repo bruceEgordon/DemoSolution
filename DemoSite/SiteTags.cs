02d525b37469bc485891075257ba2775a4cd4460
Bruce Gordon <bruce@focusls.com>
2018-08-14 17:19:39 -0400

    1

---
+++ DemoSite/SiteTags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoSite
+{
+    public class SiteTags
+    {
+        public const string Full = "full";
+        public const string Wide = "wide";
+        public const string Narrow = "narrow";
+    }
+}
